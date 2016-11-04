8525abac7ada5cb56a9a5f99e041ec1bae642e9d
Texergat33 <gatien.texereau@viacesi.fr>
2016-11-04 09:57:07 +0100

    Calculator

--- main.c
+++ main.c
@@ -9,11 +9,19 @@ int addition(int a, int b)
 {
     return a + b;
 }
-
+int division(int a, int b)
+{
+    if (b == 0)
+    {
+        printf("impossible");
+    }
+    return a / b;
+}
 int main()
 {
     printf("a + b = %d\n", addition(1,4));
     printf("a - b = %d\n", soustraction(1,4));
+    printf("a / b = %d\n", division(1,4));
 
     return 0;
 }
